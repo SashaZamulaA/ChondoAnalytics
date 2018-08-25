955eb07603d06153c55cb8e9e302f279c34eb2af
Aleksandr <z.sasha1@gmail.com>
2018-08-25 22:22:59 +0300

    refactorin

--- app/src/main/java/com/example/aleksandr/myapplication/ui/hdh/model/HDHModel.kt
+++ app/src/main/java/com/example/aleksandr/myapplication/ui/hdh/model/HDHModel.kt
@@ -1,3 +1,20 @@
 package com.example.aleksandr.myapplication.ui.hdh.model
 
-class HDHModel(val id : String, val category: String, val name: String)
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+class HDHModel {
+    var id: String = ""
+    var name: String = ""
+    var category: String = ""
+
+    constructor() {
+        //this constructor is required
+    }
+
+    constructor(id: String, name: String, category: String) {
+        this.id = id
+        this.name = name
+        this.category = category
+    }
+}
