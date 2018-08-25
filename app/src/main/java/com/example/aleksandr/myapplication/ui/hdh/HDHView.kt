efa5001a0e69333b1a4d7b6b62ffe9c22b4ede34
Aleksandr <z.sasha1@gmail.com>
2018-08-25 22:17:23 +0300

    clean code

--- app/src/main/java/com/example/aleksandr/myapplication/ui/hdh/HDHView.kt
+++ app/src/main/java/com/example/aleksandr/myapplication/ui/hdh/HDHView.kt
@@ -1,21 +1,13 @@
 package com.example.aleksandr.myapplication.ui.hdh
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.*
 import com.example.aleksandr.myapplication.BaseActivity
 import com.example.aleksandr.myapplication.R
-import com.google.firebase.database.*
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DataSnapshot
 import com.example.aleksandr.myapplication.ui.hdh.model.HDHModel
-import com.google.firebase.database.ValueEventListener
-import kotlinx.android.synthetic.main.view_hhw.*
-import android.widget.Toast
-import android.text.TextUtils
-import com.example.aleksandr.myapplication.R.id.listViewArtists
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.DatabaseReference
-import java.util.ArrayList
+import com.google.firebase.database.*
+import java.util.*
 
 
 class HDHView : BaseActivity(), IHDHView {
@@ -26,12 +18,9 @@ class HDHView : BaseActivity(), IHDHView {
     lateinit var editTextName: EditText
     lateinit var buttonSave: Button
     lateinit var spinner: Spinner
-    lateinit var databaseWord : DatabaseReference
+    lateinit var databaseWord: DatabaseReference
     lateinit var listViewArtists: ListView
     lateinit var wordList: MutableList<HDHModel>
-
-//    lateinit var wordList: ArrayList<HDHModel>
-    lateinit var adapter: WordAdapter
 
     override fun init(savedInstanceState: Bundle?) {
         super.setContentView(R.layout.view_hhw)
@@ -47,48 +36,7 @@ class HDHView : BaseActivity(), IHDHView {
         buttonSave.setOnClickListener {
             addArtist()
         }
-         }
-
-    private fun save() {
-        val name = editTextName.text.toString().trim()
-        val category = spinner.selectedItem.toString()
-//        if (name.isEmpty()) {
-//            editTextName?.error = "Please enter a name"
-//            return
-//        }
-
-        if (!TextUtils.isEmpty(name)) {
-
-            //getting a unique id using push().getKey() method
-            //it will create a unique id and we will use it as the Primary Key for our Artist
-            val id = databaseWord.push().key
-
-            //creating an Artist Object
-            val word = HDHModel(id!!, name, category)
-
-            //Saving the Artist
-            databaseWord.child(id).setValue(word)
-
-            //setting edittext to blank again
-            editTextName.setText("")
-
-            //displaying a success toast
-            Toast.makeText(this, "Artist added", Toast.LENGTH_LONG).show()
-        } else {
-            //if the value is not given displaying a toast
-            Toast.makeText(this, "Please enter a name", Toast.LENGTH_LONG).show()
-        }
-
-//        val ref = FirebaseDatabase.getInstance().getReference("word")
-//
-//        val wordId = ref.push().key
-//        val word = HDHModel(wordId!!, category, name)
-//        ref.child(wordId).setValue(word).addOnCanceledListener {
-//            Toast.makeText(applicationContext, "Successfully", Toast.LENGTH_LONG).show()
-//        }
     }
-
-//
 
     override fun onStart() {
         super.onStart()
@@ -108,18 +56,12 @@ class HDHView : BaseActivity(), IHDHView {
                         wordList.add(word)
                     }
                 }
-
                 //creating adapter
                 val artistAdapter = ArtistList(this@HDHView, wordList)
                 //attaching adapter to the listview
                 listViewArtists.adapter = artistAdapter
-
-                //attaching adapter to the listview
-
             }
-
             override fun onCancelled(databaseError: DatabaseError) {
-
             }
         })
     }
