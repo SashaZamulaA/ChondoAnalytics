07db6ca1851c91fd8efeaadb4827f391749f1db1
Aleksandr <z.sasha1@gmail.com>
2018-08-25 22:08:39 +0300

    clean code

--- app/src/main/java/com/example/aleksandr/myapplication/ui/hdh/ArtistList.java
+++ app/src/main/java/com/example/aleksandr/myapplication/ui/hdh/ArtistList.java
@@ -14,10 +14,10 @@ import java.util.List;
 
 public class ArtistList extends ArrayAdapter<HDHModel> {
     private Activity context;
-    List<HDHModel> artists;
+    private List<HDHModel> artists;
 
     public ArtistList(Activity context, List<HDHModel> artists) {
-        super(context, R.layout.item_is_word_list, artists);
+        super( context, R.layout.item_is_word_list, artists );
         this.context = context;
         this.artists = artists;
     }
@@ -26,14 +26,14 @@ public class ArtistList extends ArrayAdapter<HDHModel> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = context.getLayoutInflater();
-        View listViewItem = inflater.inflate(R.layout.item_is_word_list, null, true);
+        View listViewItem = inflater.inflate( R.layout.item_is_word_list, null, true );
 
-        TextView textViewName = listViewItem.findViewById(R.id.name);
-        TextView textViewGenre = listViewItem.findViewById(R.id.category);
+        TextView textViewName = listViewItem.findViewById( R.id.name );
+        TextView textViewGenre = listViewItem.findViewById( R.id.category );
 
-        HDHModel artist = artists.get(position);
-        textViewName.setText(artist.getName());
-        textViewGenre.setText(artist.getCategory());
+        HDHModel artist = artists.get( position );
+        textViewName.setText( artist.getName() );
+        textViewGenre.setText( artist.getCategory() );
 
         return listViewItem;
     }
