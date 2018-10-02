38b3895082d46f99293a9536473b5ed6e39e679f
Aleksandr <z.sasha1@gmail.com>
2018-10-02 11:27:17 +0300

    10.02.2018

--- app/src/main/java/com/example/aleksandr/myapplication/ui/add_task/NewNoteActivity.kt
+++ app/src/main/java/com/example/aleksandr/myapplication/ui/add_task/NewNoteActivity.kt
@@ -13,8 +13,7 @@ import kotlinx.android.synthetic.main.activity_new_note.*
 import kotlinx.android.synthetic.main.activity_new_note_copy.*
 import android.support.design.widget.AppBarLayout
 import android.support.design.widget.CollapsingToolbarLayout
-
-
+import android.support.v4.view.ViewCompat
 
 
 class NewNoteActivity : BaseActivity() {
@@ -25,23 +24,27 @@ class NewNoteActivity : BaseActivity() {
 //        val intent = intent
 //        val cheeseName = intent.getStringExtra(EXTRA_NAME)
 
-//        appbar.addOnOffsetChangedListener(object : AppBarLayout.OnOffsetChangedListener {
-//            var isShow = true
-//            var scrollRange = -1
-//
-//            override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
-//                if (scrollRange == -1) {
-//                    scrollRange = appBarLayout.totalScrollRange
-//                }
-//                if (scrollRange + verticalOffset == 0) {
-//                    collapsing_toolbar.title = "Title"
-//                    isShow = true
-////                } else if (isShow) {
-////                    collapsing_toolbar.title = "DSDSDSDSDScxc"//carefull there should a space between double quote otherwise it wont work
-////                    isShow = false
-//                }
-//            }
-//        })
+
+
+        appbar.addOnOffsetChangedListener(object : AppBarLayout.OnOffsetChangedListener {
+            var isShow = true
+            var scrollRange = -1
+
+            override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
+                ViewCompat.setElevation(appBarLayout, 10F);
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.totalScrollRange
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    collapsing_toolbar.title = "Title"
+                    isShow = true
+                } else if (isShow) {
+                    collapsing_toolbar.title = " "//carefull there should a space between double quote otherwise it wont work
+                    isShow = false
+                }
+
+            }
+        })
 //        collapsing_toolbar.title = cheeseName
 //
 //        button_add_client?.setOnClickListener {  saveNote()}
@@ -49,30 +52,30 @@ class NewNoteActivity : BaseActivity() {
 
 
 
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        return when (item?.itemId) {
-            R.id.save_note -> {
-                saveNote()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
+//    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+//        return when (item?.itemId) {
+//            R.id.save_note -> {
+//                saveNote()
+//                true
+//            }
+//            else -> super.onOptionsItemSelected(item)
+//        }
+//    }
 
-    private fun saveNote() {
+//    private fun saveNote() {
 //        val title = edit_text_title.text.toString()
 //        val achiv = edit_text_achievement.text.toString()
 //        val goal = edit_text_goal.text.toString()
-
+//
 //        if (title.trim { it <= ' ' }.isEmpty() || achiv.trim { it <= ' ' }.isEmpty()) {
 //            Toast.makeText(this, "Please insert a title and description", Toast.LENGTH_SHORT).show()
 //            return
 //        }
-
+//
 //        val notebookRef = FirebaseFirestore.getInstance()
 //                .collection("Notebook")
 //        notebookRef.add(Note(title, achiv.toDouble(), goal.toDouble()))
 //        Toast.makeText(this, "Note added", Toast.LENGTH_SHORT).show()
 //        finish()
-    }
+//    }
 }
