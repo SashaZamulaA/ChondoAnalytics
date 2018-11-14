0179a06ab68b9b4b7fa24e0f68c171cde573f4f1
SashaZamulaA <z.sasha1@gmail.com>
2018-11-14 13:26:44 +0200

    add xml in main menu

--- app/src/main/java/com/example/aleksandr/myapplication/ui/result/ResultM3AView.kt
+++ app/src/main/java/com/example/aleksandr/myapplication/ui/result/ResultM3AView.kt
@@ -3,6 +3,11 @@ package com.example.aleksandr.myapplication.ui.result
 import android.os.Bundle
 import com.example.aleksandr.myapplication.BaseActivity
 import com.example.aleksandr.myapplication.R
+import com.example.aleksandr.myapplication.ui.settings.SettingsView.Companion.AUTHOR_KEY
+import com.example.aleksandr.myapplication.ui.settings.SettingsView.Companion.QUOTE_KEY
+import com.example.aleksandr.myapplication.ui.settings.SettingsView.Companion.SPINNER
+import com.example.aleksandr.myapplication.util.FirestoreUtil
+import kotlinx.android.synthetic.main.view_m3a_result.*
 
 class ResultM3AView : BaseActivity(), IResultM3AView{
 
@@ -11,6 +16,8 @@ class ResultM3AView : BaseActivity(), IResultM3AView{
     override fun init(savedInstantState:Bundle?){
         super.setContentView(R.layout.view_m3a_result)
         presenter = ResultM3APresenter(this, application)
+
+    saveQuote()
     }
 
     override fun onAttachedToWindow() {
@@ -25,6 +32,26 @@ class ResultM3AView : BaseActivity(), IResultM3AView{
 
     override fun setButtonVisibility(isVisible: Boolean) {
 //        btn_info.visibility = isVisible.toAndroidVisibility()
+    }
+
+    private fun saveQuote() {
+        val intro = introduction_edittext.text.toString()
+        val oneDayWS = one_day_seminar_edittext.text.toString()
+
+        if (intro.isEmpty() || oneDayWS.isEmpty()) {
+            return
+        }
+        val dataToSave = HashMap<String, Any>()
+        dataToSave[INRO] = intro
+        dataToSave[ONEDAYWS] = oneDayWS
+
+        FirestoreUtil.currentUserDocRef.set(dataToSave).addOnSuccessListener {
+        }
+    }
+    companion object {
+        val INRO = "intriduction"
+        val ONEDAYWS = "oneDayWS"
+        
     }
 
 }
