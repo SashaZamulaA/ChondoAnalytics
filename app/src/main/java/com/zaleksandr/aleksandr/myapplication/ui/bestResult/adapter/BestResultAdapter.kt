1b9b483a771ccef9f4afcd5476c1c2bfb0fd3016
SashaZamulaA <z.sasha1@gmail.com>
2019-03-12 16:16:14 +0200

    11.03.2019 + Add best logic all + photo

--- app/src/main/java/com/zaleksandr/aleksandr/myapplication/ui/bestResult/adapter/BestResultAdapter.kt
+++ app/src/main/java/com/zaleksandr/aleksandr/myapplication/ui/bestResult/adapter/BestResultAdapter.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatDrawableManager
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.zaleksandr.aleksandr.myapplication.R
 import com.zaleksandr.aleksandr.myapplication.model.City
 import com.zaleksandr.aleksandr.myapplication.ui.eachCentersResult.adapter.EachCenterAdapter
@@ -19,6 +22,7 @@ class BestResultAdapter(private val context: Context,
                         private var pairList: List<Pair<String?, Int>>
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
+
     private val noteRefCollection = firestoreInstance.collection("NewCity")
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(com.zaleksandr.aleksandr.myapplication.R.layout.item_best_result_list,
@@ -26,10 +30,26 @@ class BestResultAdapter(private val context: Context,
         return CentersHolder(v)
     }
 
-    fun setList(pairList: List<Pair<String?, Int>>) {
-        this.pairList = pairList
-        notifyDataSetChanged()
+    fun <I> RecyclerView.Adapter<*>.updateListWithDiffs(oldList: List<I>, newList: List<I>, listSetter: (List<I>) -> Unit, itemsTheSame: (I, I) -> Boolean = { o, n -> o == n }, contentsTheSame: (I, I) -> Boolean = { o, n -> o == n }, detectMoves: Boolean = false) {
+        DiffUtil.calculateDiff(object : DiffUtil.Callback() {
+            override fun getOldListSize(): Int = oldList.size
+            override fun getNewListSize(): Int = newList.size
+
+            override fun areItemsTheSame(oi: Int, ni: Int): Boolean = itemsTheSame(oldList[oi], newList[ni])
+            override fun areContentsTheSame(oi: Int, ni: Int): Boolean = contentsTheSame(oldList[oi], newList[ni])
+            override fun getChangePayload(oi: Int, ni: Int): Any? = Unit
+        }, detectMoves).also {
+            listSetter(newList)
+            it.dispatchUpdatesTo(this)
+        }
     }
+
+    fun setList(pairList: List<Pair<String?, Int>>) = updateListWithDiffs(
+            this.pairList, pairList,
+            { this.pairList = pairList },
+            { o, n -> o.first == n.first },
+            detectMoves = true)
+
 
     override fun getItemCount(): Int {
         return pairList.size
@@ -80,10 +100,20 @@ class BestResultAdapter(private val context: Context,
 
                         val resultNote = documentSnapshot.toObject(City::class.java)
 
+
+                            if (resultNote.name == item.first) {
+                                GlideApp.with(context)
+                                        .load(resultNote.userPhotoPath.let { StorageUtil.pathToReference(it!!) })
+                                        .placeholder(R.drawable.ic_account_circle_black_24dp)
+                                        .circleCrop()
+                                        .into(itemView.best_profile_picture)
+
+                        }
+
                         if (!resultNote.intro.isNullOrEmpty()) {
                             if (resultNote.name == item.first) {
                                 val intro = (Integer.parseInt(resultNote.intro))
-                                           sumIntro += intro
+                                sumIntro += intro
                             }
                         }
 
@@ -100,28 +130,21 @@ class BestResultAdapter(private val context: Context,
                                 sumTwoDay += twoDay
                             }
                         }
-                            if (!resultNote.twOneDay.isNullOrEmpty()) {
-                                if (resultNote.twOneDay == item.first) {
-                                    val twoOneDay = (Integer.parseInt(resultNote.twOneDay))
-                                    sumTwoOneDay += twoOneDay
-                                }
-                            }
-
-                            if (!resultNote.nwet.isNullOrEmpty()) {
-                                if (resultNote.nwet == item.first) {
-                                    val nwet = (Integer.parseInt(resultNote.nwet))
-                                    sumNWET += nwet
-                                }
-                            }
-
-                        if (resultNote.name == item.first) {
-                            if (!resultNote.userPhotoPath.isNullOrBlank()) {
-                                GlideApp.with(context)
-                                        .load(resultNote.userPhotoPath.let { StorageUtil.pathToReference(it) })
-                                        .circleCrop()
-                                        .into(itemView.best_profile_picture)
+                        if (!resultNote.twOneDay.isNullOrEmpty()) {
+                            if (resultNote.twOneDay == item.first) {
+                                val twoOneDay = (Integer.parseInt(resultNote.twOneDay))
+                                sumTwoOneDay += twoOneDay
                             }
                         }
+
+                        if (!resultNote.nwet.isNullOrEmpty()) {
+                            if (resultNote.nwet == item.first) {
+                                val nwet = (Integer.parseInt(resultNote.nwet))
+                                sumNWET += nwet
+                            }
+                        }
+
+
 
 //                        if (layoutPosition == 0 ||layoutPosition == 1 ||layoutPosition == 2 ){
 //                            itemView.best_cardView.setBackgroundResource(R.color.best3_color)
@@ -132,22 +155,22 @@ class BestResultAdapter(private val context: Context,
 //                            itemView.best_cardView.setBackgroundResource(R.color.best3_color)
 //                        }
 
-                        if (resultNote.name == item.first){
-                            itemView.best_number_position.text = (layoutPosition+1).toString()
+                        if (resultNote.name == item.first) {
+                            itemView.best_number_position.text = (layoutPosition + 1).toString()
                         }
 
-                            itemView.best_result_name.text = item.first.toString()
-                            itemView.best_result.text = item.second.toString()
+                        itemView.best_result_name.text = item.first.toString()
+                        itemView.best_result.text = item.second.toString()
 
-                        }
-
-                        itemView.best_res_intro.text = sumIntro.toString()
-                        itemView.best_result_one_day.text = sumOneDay.toString()
-                        itemView.best_res_two_day.text = sumTwoDay.toString()
-                        itemView.best_res_twoOne_day.text = sumTwoOneDay.toString()
-                        itemView.best_res_nwet.text = sumNWET.toString()
                     }
+
+                    itemView.best_res_intro.text = sumIntro.toString()
+                    itemView.best_result_one_day.text = sumOneDay.toString()
+                    itemView.best_res_two_day.text = sumTwoDay.toString()
+                    itemView.best_res_twoOne_day.text = sumTwoOneDay.toString()
+                    itemView.best_res_nwet.text = sumNWET.toString()
                 }
             }
         }
     }
+}
